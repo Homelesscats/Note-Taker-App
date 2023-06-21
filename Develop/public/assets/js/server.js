17e2a0234ea8372a2226da6dd1534b74c9aa5a20
homelesscat <gilbertperezdt@gmail.com>
2023-06-21 16:51:26 -0700

    Server.js file, entered dependencies

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -4,3 +4,14 @@
     const port = 3001;
 
     const app = express();
+
+    app.use(express.urlencoded({ extended: false}));
+    app.use(express.json());
+    app.use(express.static("public"));
+    app.use(html_routes);
+    app.use(api_routes);
+
+    app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    });
+
